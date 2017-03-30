b58cf6f66697f33583b5b0234dc26c58dfe4fbb5
picholasnaridon <paridonn1@gmail.com>
2017-03-30 10:58:25 -0400

    hound

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,7 +1,6 @@
 class DecksController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authorize_user, except: [:index, :show]
-
   def index
     @decks = Deck.all
   end
